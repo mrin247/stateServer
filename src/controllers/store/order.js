7de3a2f3d817a12ca01ca27cb48ab0fc02e75e3a
Mrin <mrinmoym247@gmail.com>
2022-04-08 15:55:57 +0530

    Send Orders as an array

--- src/controllers/store/order.js
+++ src/controllers/store/order.js
@@ -3,45 +3,6 @@ const Error = require("../../utils/errResponse");
 const Order = require("../../models/Order");
 const Address = require("../../models/Address");
 
-// exports.updateOrder = (req, res) => {
-//   Order.updateOne(
-//     { _id: req.body.orderId, "items.$.productId": req.body.itemId },
-//     {
-//       $set: {
-//         "orderStatus.$": [
-//           { type: req.body.type, date: new Date(), isCompleted: true },
-//         ],
-//       },
-//     },
-//     {
-//       upsert: true,
-//     }
-//   ).exec((error, order) => {
-//     if (error) return res.status(400).json({ error });
-//     if (order) {
-//       res.status(201).json({ order });
-//     }
-//   });
-// };
-
-exports.getCustomerOrders = async (req, res, next) => {
-  const sellerId = req.user._id;
-  try {
-    const orders = await Order.find({ "items.sellerId": sellerId }).populate(
-      "items.productId",
-      "name"
-    );
-
-    if (orders) {
-      res.status(200).json({ orders });
-    } else {
-      return new Error("No order record found", 400);
-    }
-  } catch (error) {
-    next(error);
-  }
-};
-
 exports.updateOrder = async (req, res, next) => {
   const { orderId, productId } = req.body;
   try {
@@ -59,6 +20,29 @@ exports.updateOrder = async (req, res, next) => {
     );
     if (order) {
       res.status(200).json({ order });
+    } else {
+      return new Error("No order record found", 400);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getCustomerOrders = async (req, res, next) => {
+  const sellerId = req.user._id;
+  try {
+    const orders = await Order.find({ "items.sellerId": sellerId })
+      .select("items")
+      .populate("items.productId", "name");
+
+    if (orders) {
+      let itemArray = [];
+      orders.map((order) => {
+        order.items.map((items) => {
+          itemArray.push(items);
+        });
+      });
+      res.status(200).json({ orders: itemArray });
     } else {
       return new Error("No order record found", 400);
     }
